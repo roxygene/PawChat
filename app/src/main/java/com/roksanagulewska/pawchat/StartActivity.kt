0750c6b49e248d29aeb10ba3526e420dd34b23a2
Roksana <rgulewska@gmail.com>
2023-09-25 21:32:48 +0200

    Created sign in and sign up activities

--- app/src/main/java/com/roksanagulewska/pawchat/StartActivity.kt
+++ app/src/main/java/com/roksanagulewska/pawchat/StartActivity.kt
@@ -1,11 +1,35 @@
 package com.roksanagulewska.pawchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val btnSignIn = findViewById<Button>(R.id.btn_sign_in)
+        val btnSignUp = findViewById<Button>(R.id.btn_sign_up)
+        val btnGoogle = findViewById<Button>(R.id.btn_google)
+
+        btnSignIn.setOnClickListener{
+            Intent(this, SignInActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        btnSignUp.setOnClickListener{
+            Intent(this, SignUpActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        btnSignIn.setOnClickListener{
+           
+        }
+
     }
+
 }
